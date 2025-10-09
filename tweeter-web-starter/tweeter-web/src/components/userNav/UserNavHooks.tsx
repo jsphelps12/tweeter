e889f41c36998d62d25bd51869ce9758391b9466
jsphelps12 <jsphelps12@gmail.com>
2025-10-08 21:28:17 -0700

    2A UserNavHooks

--- tweeter-web-starter/tweeter-web/src/components/userNav/UserNavHooks.tsx
+++ tweeter-web-starter/tweeter-web/src/components/userNav/UserNavHooks.tsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useMessageActions } from "../toaster/MessageHooks";
 import { useUserInfo, useUserInfoActions } from "../userInfo/UserInfoHooks";
-import { AuthToken, FakeData, User } from "tweeter-shared";
+import { UserNavHooksPresenter, UserNavView } from "../../presenter/UserNavHooksPresenter";
+import { useRef } from "react";
 
 export const useUserNavigation = (featurePath: string) => {
   const { updateUserInfo, setDisplayedUser } = useUserInfoActions();
@@ -9,38 +10,38 @@ export const useUserNavigation = (featurePath: string) => {
   const { displayErrorMessage } = useMessageActions();
   const navigate = useNavigate();
 
+  const listener: UserNavView = {
+      displayErrorMessage: displayErrorMessage, 
+      setDisplayedUser: setDisplayedUser, 
+      navigate: navigate
+    }
+  
+    const presenterRef = useRef<UserNavHooksPresenter | null>(null);
+        if (presenterRef.current === null) {
+          presenterRef.current = new UserNavHooksPresenter(listener);
+        }
+
   const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
     event.preventDefault();
 
-    try {
-      const alias = extractAlias(event.target.toString());
+    presenterRef.current!.navigateToUser(event, authToken!, displayedUser!, featurePath);
 
-      const toUser = await getUser(authToken!, alias);
+    // try {
+    //   const alias = presenterRef.current!.extractAlias(event.target.toString());
 
-      if (toUser) {
-        if (!toUser.equals(displayedUser!)) {
-          setDisplayedUser(toUser);
-          navigate(`${featurePath}/${toUser.alias}`);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to get user because of exception: ${error}`
-      );
-    }
+    //   const toUser = await presenterRef.current!.getUser(authToken!, alias);
+
+    //   if (toUser) {
+    //     if (!toUser.equals(displayedUser!)) {
+    //       setDisplayedUser(toUser);
+    //       navigate(`${featurePath}/${toUser.alias}`);
+    //     }
+    //   }
+    // } catch (error) {
+    //   displayErrorMessage(
+    //     `Failed to get user because of exception: ${error}`
+    //   );
+    // }
   };
   return navigateToUser;
 }
-
-const extractAlias = (value: string): string => {
-  const index = value.indexOf("@");
-  return value.substring(index);
-};
-
-const getUser = async (
-  authToken: AuthToken,
-  alias: string
-): Promise<User | null> => {
-  // TODO: Replace with the result of calling server
-  return FakeData.instance.findUserByAlias(alias);
-};
---
+++ tweeter-web-starter/tweeter-web/src/presenter/UserNavHooksPresenter.ts
@@ -0,0 +1,54 @@
+import { AuthToken, User } from "tweeter-shared";
+import { UserService } from "../model.service/UserService";
+import { NavigateFunction } from "react-router-dom";
+
+export interface UserNavView{
+    setDisplayedUser: (user: User) => void;
+    displayErrorMessage: (message: string) => void;
+    navigate: NavigateFunction;
+}
+
+export class UserNavHooksPresenter {
+    private userService: UserService;
+    private _view: UserNavView;
+    
+    constructor(view: UserNavView){
+        this.userService = new UserService();
+        this._view = view;
+    }
+
+
+    public async getUser (
+        authToken: AuthToken,
+        alias: string
+        ): Promise<User | null> {
+        return this.userService.getUser(authToken, alias);
+        };
+
+    public extractAlias (value: string): string {
+        const index = value.indexOf("@");
+        return value.substring(index);
+    };
+
+    public async navigateToUser (event: React.MouseEvent, authToken: AuthToken, displayedUser: User, featurePath: string): Promise<void> {
+
+        try {
+        const alias = this.extractAlias(event.target.toString());
+
+        const toUser = await this.getUser(authToken, alias);
+
+        if (toUser) {
+            if (!toUser.equals(displayedUser!)) {
+            this._view.setDisplayedUser(toUser);
+            this._view.navigate(`${featurePath}/${toUser.alias}`);
+            }
+        }
+        } catch (error) {
+        this._view.displayErrorMessage(
+            `Failed to get user because of exception: ${error}`
+        );
+        }
+    };
+
+
+}
